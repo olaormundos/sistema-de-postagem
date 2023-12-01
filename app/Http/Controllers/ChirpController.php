0c0c3c1a5662655cd5939680d4ab320df25b9178
olaormundos <vanderleidasilvaormundo@gmail.com>
2023-11-30 21:53:20 -0300

    Editando mensagens

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -16,7 +16,6 @@ class ChirpController extends Controller
     public function index(): Response */
     public function index(): View
     {
-        //
         return view('chirps.index', [
             'chirps' => Chirp::with('user')->latest()->get(),
         ]);
@@ -36,7 +35,6 @@ class ChirpController extends Controller
 
     public function store(Request $request): RedirectResponse
     {
-        //
         $validated = $request->validate([
             'message' => 'required|string|max:255',
         ]);
@@ -57,17 +55,29 @@ class ChirpController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Chirp $chirp)
+    public function edit(Chirp $chirp): View
     {
-        //
+        $this->authorize('update', $chirp);
+ 
+        return view('chirps.edit', [
+            'chirp' => $chirp,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Chirp $chirp)
+    public function update(Request $request, Chirp $chirp): RedirectResponse
     {
-        //
+        $this->authorize('update', $chirp);
+ 
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $chirp->update($validated);
+ 
+        return redirect(route('chirps.index'));
     }
 
     /**
